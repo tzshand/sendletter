db6f4112e0d072e8180326fbbab4b8f282cf2d19
Colin Shand <colinh.shand@gmail.com>
2026-03-23 13:12:41 -0400

    Switch to @next/third-parties for GA, the official Next.js approach

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,34 +1,13 @@
-"use client";
-
-import { useEffect } from "react";
-
-const GA_ID = "G-39L40L4RTY";
-const AW_ID = "AW-11542356574";
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare global {
-  interface Window {
-    dataLayer?: any[];
-    gtag?: (...args: any[]) => void;
-  }
-}
+import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 export function Analytics() {
-  useEffect(() => {
-    window.dataLayer = window.dataLayer || [];
-    const gtag: Window["gtag"] = function () {
-      window.dataLayer!.push(arguments);
-    };
-    window.gtag = gtag;
-    gtag("js", new Date());
-    gtag("config", GA_ID);
-    gtag("config", AW_ID);
-
-    const script = document.createElement("script");
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
-    script.async = true;
-    document.head.appendChild(script);
-  }, []);
-
-  return null;
+  return (
+    <>
+      <GoogleAnalytics gaId="G-39L40L4RTY" />
+      <Script id="gtag-ads" strategy="afterInteractive">
+        {`window.gtag && window.gtag('config', 'AW-11542356574');`}
+      </Script>
+    </>
+  );
 }
